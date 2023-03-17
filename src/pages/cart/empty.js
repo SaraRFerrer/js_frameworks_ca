3e7e7e7f8439c6d53c0b470bb5f7317d35e52cc0
SaraRFerrer <sararferrer@hotmail.com>
2023-03-17 19:05:42 +0100

    working remove

--- src/pages/cart/empty.js
+++ src/pages/cart/empty.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import styles from "../../styleModules/emptyCart.module.css";
 
 function EmptyCart() {
   return (
     <div>
       <h1>Your cart is empty</h1>
-      <div>
+      <div className={styles.container}>
         <Link to={"/"}>
-          <button>Continue Shopping</button>
+          <button className={styles.emptyBtn}>Continue Shopping</button>
         </Link>
       </div>
     </div>
